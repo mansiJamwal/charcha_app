e46b7cc1d0518dd04e541d5b20b2088c76eedef3
Aad1tya27 <agarwal.aaditya2765@gmail.com>
2024-09-23 14:02:02 +0530

    typo

--- client/src/components/LandingPage.tsx
+++ client/src/components/LandingPage.tsx
@@ -4,7 +4,7 @@ const LandingPage = () => {
   return (
     <main className="h-screen bg-gradient-to-bl from-black to-gray-900 flex gap-[20px] flex-col items-center justify-center text-white ">
         <div className="text-[40px] ">Welcome to  <span className="font-semibold">Charcha, The Chat App</span></div>
-        <div className="w-[70%] text-center text-xl text-gray-200">This is a social media application, where you can interact with the all the Users on this Platform.
+        <div className="w-[70%] text-center text-xl text-gray-200">This is a social media application, where you can interact with all the Users on this Platform.
          <div className="buttons flex justify-center m-8 gap-5">
             <NavLink to={"/signup"} className="text-black font-medium bg-white p-2 px-4 rounded-[6px]">Sign Up</NavLink>
             <NavLink to={"/signin"} className="text-black font-medium bg-white p-2 px-4 rounded-[6px]">Sign In</NavLink>
